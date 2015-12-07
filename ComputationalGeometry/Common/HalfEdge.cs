4a5971449a967f0624939815d8958c84596f87aa
Thanh <phamhathanh130394@gmail.com>
2015-12-07 21:45:12 +0700

    initial commit

---
+++ ComputationalGeometry/Common/HalfEdge.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputationalGeometry.Common
+{
+    public class HalfEdge
+    {
+        public Vertex Origin { get; set; }
+        public HalfEdge Twin { get; set; }
+        public HalfEdge Next { get; set; }
+        public HalfEdge Previous { get; set; }
+        public Face Face { get; set; }
+    }
+}
---
+++ ComputationalGeometry/Common/Vertex.cs
@@ -0,0 +1,29 @@
+﻿namespace ComputationalGeometry.Common
+{
+    public class Vertex
+    {
+        private readonly double x, y;
+
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+
+        public Vertex(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
