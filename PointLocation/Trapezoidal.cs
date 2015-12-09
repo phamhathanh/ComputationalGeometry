109696bb3218e190dc0f5b9cd31c3d1a4a0e6540
Lê Văn Chiến <chienlv.kstntoantink57@gmail.com>
2015-12-09 17:49:47 +0700

    Thêm class Algorithm vào project common.

---
+++ PointLocation/Trapezoidal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputationalGeometry.Common;
+
+namespace ComputationGeometry
+{
+    public class Trapezoidal
+    {
+        public Vertex Leftp { get; set; }
+        public Vertex Rightp { get; set; }
+        public HalfEdge Top { get; set; }
+        public HalfEdge Bottom { get; set; }
+        public Trapezoidal RightTopNeighbor { get; set; }
+        public Trapezoidal LeftTopNeighbor { get; set; }
+        public Trapezoidal LeftBottomNeighbor { get; set; }
+        public Trapezoidal RightBottomNeighbor { get; set; }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputationalGeometry.Common;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommonTest();
+
+            Console.ReadLine();
+        }
+
+        private static void CommonTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (int index in Algorithm.RandomPermutation(5))
+                {
+                    Console.Write(index);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
