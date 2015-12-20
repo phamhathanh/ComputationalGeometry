db35c5bedb116664ac5c204c5a40e889bda36a2c
Thanh <phamhathanh130394@gmail.com>
2015-12-21 00:26:10 +0700

    basic roadmap

--- MotionPlanning/RoadMap.cs
+++ MotionPlanning/RoadMap.cs
@@ -31,7 +31,64 @@ namespace ComputationalGeometry.MotionPlanning
 
         private Vector2 GetCenterPoint(ITrapezoid trapezoid)
         {
+            double leftEdgeX = trapezoid.LeftEdge.XPosition,
+                rightEdgeX = trapezoid.RightEdge.XPosition,
+                centerX = (leftEdgeX + rightEdgeX) / 2;
+
             throw new NotImplementedException();
+        }
+
+        public IEnumerable<Vector2> CalculatePath(Vector2 start, Vector2 goal)
+        {
+            ITrapezoid startingTrapezoid = trapezoidalMap.PointLocation(start),
+                        finalTrapezoid = trapezoidalMap.PointLocation(goal);
+            Junction startingJunction = junctionByTrapezoid[startingTrapezoid],
+                        finalJunction = junctionByTrapezoid[finalTrapezoid];
+
+            yield return start;
+
+            foreach (var junction in SearchForPath(startingJunction, finalJunction))
+                yield return junction.Position;
+
+            yield return goal;
+        }
+
+        private IEnumerable<Junction> SearchForPath(Junction start, Junction goal)
+        {
+            var previous = new Dictionary<Junction, Junction>();
+            var queue = new Queue<Junction>();
+
+            queue.Enqueue(start);
+
+            while (queue.Count != 0)
+            {
+                var current = queue.Dequeue();
+                
+                if (current == goal)
+                {
+                    var path = new Stack<Junction>();
+                    do
+                    {
+                        path.Push(current);
+                        current = previous[current];
+                    }
+                    while (current != start);
+                    while (path.Count != 0)
+                        yield return path.Pop();
+                }
+
+                foreach (var nextStop in current.Neighbors)
+                {
+                    var isVisited = previous.ContainsKey(nextStop);
+                    if (isVisited)
+                        continue;
+                    
+                    queue.Enqueue(nextStop);
+                    previous.Add(nextStop, current);
+                }
+            }
+
+            yield break;
         }
     }
 }
